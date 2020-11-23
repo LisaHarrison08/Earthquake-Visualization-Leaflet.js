ca68085b4ea521bd0a58cd944800c99c7da74ef1
Lisa Harrison <lmharrison0810@gmail.com>
2020-11-22 22:51:32 -0500

    config file included

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibG1oODAiLCJhIjoiY2tnZnRlbGF5MWptdzJxcW8wZzl0enQwZCJ9.spItYFgcaqoqxlFz8VvBsQ";
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,5 @@
 // Store our API endpoint inside queryUrl
+// Data set: 'All Earthquakes from the Past 7 Days'
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
@@ -28,6 +29,7 @@ function markerSize(mag) {
 }
 function createFeatures(earthquakeData) {
 
+
   var earthquakes = L.geoJSON(earthquakeData, {
 
     // Define a function we want to run once for each feature in the features array // Give each feature a popup describing the place and time of the earthquake
@@ -35,7 +37,8 @@ function createFeatures(earthquakeData) {
 
       layer.bindPopup("<h3>" + feature.properties.place +
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude: " + feature.properties.mag + "</p>")
-    }, pointToLayer: function (feature, latlng) {
+    },
+    pointToLayer: function (feature, latlng) {
       return new L.circle(latlng,
         {
           radius: markerSize(feature.properties.mag),
